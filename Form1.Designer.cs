0504e57a9cfad22b9bc4eda117bdfc889186753a
SanceiLaks <39582793+Sanceilaks@users.noreply.github.com>
2020-11-12 18:58:48 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace DiagramDrawer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DrawPanel = new System.Windows.Forms.Panel();
+            this.FirstNumber = new System.Windows.Forms.NumericUpDown();
+            this.SecondNumber = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.FirstNumber)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SecondNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DrawPanel
+            // 
+            this.DrawPanel.Location = new System.Drawing.Point(12, 136);
+            this.DrawPanel.Name = "DrawPanel";
+            this.DrawPanel.Size = new System.Drawing.Size(684, 355);
+            this.DrawPanel.TabIndex = 0;
+            this.DrawPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawPanel_Paint);
+            // 
+            // FirstNumber
+            // 
+            this.FirstNumber.Location = new System.Drawing.Point(12, 12);
+            this.FirstNumber.Name = "FirstNumber";
+            this.FirstNumber.Size = new System.Drawing.Size(120, 20);
+            this.FirstNumber.TabIndex = 0;
+            this.FirstNumber.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.FirstNumber.ValueChanged += new System.EventHandler(this.FirstNumber_ValueChanged);
+            // 
+            // SecondNumber
+            // 
+            this.SecondNumber.Location = new System.Drawing.Point(138, 12);
+            this.SecondNumber.Name = "SecondNumber";
+            this.SecondNumber.Size = new System.Drawing.Size(120, 20);
+            this.SecondNumber.TabIndex = 1;
+            this.SecondNumber.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.SecondNumber.ValueChanged += new System.EventHandler(this.SecondNumber_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(708, 503);
+            this.Controls.Add(this.SecondNumber);
+            this.Controls.Add(this.FirstNumber);
+            this.Controls.Add(this.DrawPanel);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.FirstNumber)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SecondNumber)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewButtonColumn Column1;
+        private System.Windows.Forms.DataGridViewButtonColumn Column2;
+        private System.Windows.Forms.Panel DrawPanel;
+        private System.Windows.Forms.NumericUpDown FirstNumber;
+        private System.Windows.Forms.NumericUpDown SecondNumber;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiagramDrawer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DrawDiagram(Graphics g, Pen pen, int x, int y, int radius, int[] triangelsValues, int triagnelsNum)
+        {
+            void drawDiagram(int[] _triangelsAngels, int _triangleAmmount, int _triagnelsNum)
+            {
+                int startPos = 0;
+                const double twicePI = Math.PI * 2.0d;
+                List<PointF> points = new List<PointF>();
+
+                for (int i = 0; i < _triagnelsNum; i++)
+                {
+                    points.Add(new PointF(x, y));
+
+                    for (int j = startPos; j <= startPos + _triangelsAngels[i]; j++)
+                    {
+                        double t = (twicePI * j / _triangleAmmount) + Math.PI;
+                        points.Add(new PointF(Convert.ToSingle(x - Math.Sin(t) * radius), Convert.ToSingle(y + Math.Cos(t) * radius)));
+                    }
+
+                    startPos += _triangelsAngels[i];
+                }
+
+                g.DrawPolygon(pen, points.ToArray());
+            };
+
+            int triangleAmmount = 0;  //Ammount of triangels for draw circle
+            int totalValues = 0;
+            int[] triangelsAngels = new int[triagnelsNum];
+
+            triangelsAngels[0] = 100;
+
+            for (int i = 0; i < triagnelsNum; i++)
+            {
+                totalValues += triangelsValues[i];
+            }
+
+            if (totalValues != 100)
+            {
+                drawDiagram(new int[1] { 100 }, 100, 1);
+                return;
+            }
+
+            for (int i = 0; i < triagnelsNum; i++)
+            {
+                triangelsAngels[i] = (int)((float)triangelsValues[i] / (float)totalValues * 100);
+            }
+
+            for (int i = 0; i < triagnelsNum; i++)
+            {
+                triangleAmmount += triangelsAngels[i];
+            }
+
+            drawDiagram(triangelsAngels, triangleAmmount, triagnelsNum);
+        }
+
+        private void DrawPanel_Paint(object sender, PaintEventArgs e)
+        {
+            var panel = sender as Panel;
+
+            using (Graphics g = e.Graphics)
+            {
+                Pen redPen = new Pen(Color.Red, 2);
+                Pen bluePen = new Pen(Color.Blue, 2);
+
+                DrawDiagram(g, redPen, 100, 100, 100, new int[] { (int)FirstNumber.Value, (int)SecondNumber.Value }, 2);
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawPanel.Refresh();
+        }
+
+        private void FixValue(int first, int second)
+        {
+            if (first + second != 100)
+            {
+                if (first > second)
+                    second = 100 - first;
+                else
+                    first = 100 - second;
+            }
+        }
+
+        private void FirstNumber_ValueChanged(object sender, EventArgs e)
+        {
+            FixValue((int)FirstNumber.Value, (int)SecondNumber.Value);
+            DrawPanel.Refresh();
+        }
+
+        private void SecondNumber_ValueChanged(object sender, EventArgs e)
+        {
+            FixValue((int)FirstNumber.Value, (int)SecondNumber.Value);
+            DrawPanel.Refresh();
+        }
+    }
+}
